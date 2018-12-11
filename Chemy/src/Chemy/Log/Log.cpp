53db3157e4270e4b4a8d8a2aceb2dc020c1a1084
DaVonte Joseph Carter Vault <supremerula@live.com>
2018-12-10 23:42:39 -0500

    ECS added. All Bugs Fixed.
    -Need to Export all DLL classes and functions

---
+++ Chemy/src/Chemy/Log/Log.cpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Log.h"
+#include "spdlog\sinks\stdout_color_sinks.h"
+
+namespace Chemy {
+
+
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+	void Log::Init() {
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("CHEMY");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+
+		s_ClientLogger = spdlog::stdout_color_mt("APPLICATION");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+	}
+
+}
---
+++ Chemy/src/Chemy/Log/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include "Chemy\Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Chemy {
+	
+	class CHEMY_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+
+// Core log macros
+#define CZ_CORE_TRACE(...)    ::Chemy::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CZ_CORE_INFO(...)     ::Chemy::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CZ_CORE_WARN(...)     ::Chemy::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CZ_CORE_ERROR(...)    ::Chemy::Log::GetCoreLogger()->error(__VA_ARGS__)
+
+// Client log macros
+#define CZ_TRACE(...)	      ::Chemy::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CZ_INFO(...)	      ::Chemy::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CZ_WARN(...)	      ::Chemy::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CZ_ERROR(...)	      ::Chemy::Log::GetClientLogger()->error(__VA_ARGS__)
